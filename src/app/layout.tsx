f230dae0ab3ec74f72690518bd4195c14517ed06
duong.dev <phamtanduongtk29@gmail.com>
2024-09-19 23:36:51 +0700

    Add OG on global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 // Styles
@@ -6,24 +5,13 @@ import "@app/styles/globals.css";
 // UI
 import { Box, Text, ThemeProvider } from "@app/ui";
 // Constants
-import { APP_TITLE, WARNING_MESSAGE } from "@app/constants";
+import { WARNING_MESSAGE } from "@app/constants";
 
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-inter",
 });
-
-export const metadata: Metadata = {
-  title: APP_TITLE,
-  description:
-    "Our Task Management platform is designed to streamline productivity and keep teams organized. With intuitive task creation, real-time tracking, and customizable workflows, users can efficiently manage projects from start to finish. The platform enables collaboration, allowing team members to assign tasks, set deadlines, and monitor progress in one centralized hub. Whether you're working solo or with a team, our tool ensures that tasks are prioritized, deadlines are met, and productivity stays on track",
-  icons: [
-    {
-      url: "/icons/logo.svg",
-    },
-  ],
-};
 
 const MainLayout = ({
   children,
